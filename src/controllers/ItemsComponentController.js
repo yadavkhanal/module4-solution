310e34766203fbd026a7b7a1c0dcdb0d6dc59896
@ykhanal <ykhanal@ztr.com>
2017-10-17 21:47:21 -0400

    finalized

--- src/controllers/ItemsComponentController.js
+++ src/controllers/ItemsComponentController.js
@@ -5,6 +5,7 @@
 	ItemsComponentController.$inject = ['items'];
 	function ItemsComponentController(items){
 		var $ctrl = this;
-		$ctrl.items = items.data.menu_items;		
+		$ctrl.items = items.data.menu_items;	
+		$ctrl.category = items.data.category.name;
 	}
 })();
